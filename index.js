5d556a89f28f342126256ef5b35cb24749d597bc
Miguel Jimenez <migang9@gmail.com>
2021-07-02 00:29:49 -0400

    spotter part 2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP,  fetchCoordsByIP } = require('./iss');
+
+let ip = '';
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -8,3 +10,16 @@ fetchMyIP((error, ip) => {
 
   console.log('It worked! Returned IP:' , ip);
 });
+
+fetchCoordsByIP(ip, (error, coords) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log('It worked! Returned ', coords);
+});
+
+//fetchCoordsByIP(fetchMyIP(callback));
+// call the function fetchCoordsByIP to get coordinates by ip address
+// fetchCoordsByIP(ip,callback) {
+// }
--- iss.js
+++ iss.js
@@ -26,4 +26,26 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+//`https://freegeoip.app/json/${ip}
+// https://freegeoip.app/json/invalidIPHere
+//let ip = "72.141.98.17";
+
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+};
+//console.log(fetchMyIP());
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
